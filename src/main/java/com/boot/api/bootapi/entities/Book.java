0b05ac4e3d9b2eb019334f16aa1d690c27f39f56
rahuldora71 <rahuldoraksp02@gmail.com>
2024-05-24 22:54:38 +0530

    Use One to One Mapping And Use  @JsonManagedReference, @JsonBackReference Annotation

--- src/main/java/com/boot/api/bootapi/entities/Book.java
+++ src/main/java/com/boot/api/bootapi/entities/Book.java
@@ -1,9 +1,13 @@
 package com.boot.api.bootapi.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Book {
@@ -11,8 +15,11 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String title;
-    private String author;
-    public Book(int id, String title, String author) {
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Author author;
+    public Book(int id, String title, Author author) {
         this.id = id;
         this.title = title;
         this.author = author;
@@ -31,10 +38,10 @@ public class Book {
     public void setTitle(String title) {
         this.title = title;
     }
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
     @Override
